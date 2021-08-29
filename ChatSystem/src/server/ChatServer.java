76c007ef491894fbcd510289d078b69d696d87b3
Ying Mao <yingmao50@gmail.com>
2021-08-29 15:52:56 +1000

    Implemented generating NewIdentity for new clients

--- ChatSystem/src/server/ChatServer.java
+++ ChatSystem/src/server/ChatServer.java
@@ -1,13 +1,19 @@
 package server;
 
+import com.google.gson.Gson;
+import command.NewIdentityCommand;
+
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 
 
 public class ChatServer {
     public static final int port = 6379;
+    private int identityCount = 1;
+    private final Gson gson = new Gson();
     private static final ChatManager chatManager = new ChatManager();
     private static final CommandFactory commandFactory = new CommandFactory();
     private boolean alive;
@@ -17,6 +23,33 @@ public class ChatServer {
         new ChatServer().handle();
     }
 
+    private void generateIdentityForNewClient(ServerConnection serverConnection){
+        String newID = autoGenerateIdentity();
+        serverConnection.setName(newID);
+        NewIdentityCommand newIdentityCommand = new NewIdentityCommand("", newID);
+
+        String jsonMessage = gson.toJson(newIdentityCommand);
+        chatManager.sendToOneClient(jsonMessage, serverConnection);
+    }
+
+    private synchronized String autoGenerateIdentity(){
+        String identity = "guest" + identityCount;
+        while (isIdentityInList(identity)){
+            identityCount += 1;
+            identity = "guest" + identityCount;
+        }
+        return identity;
+    }
+
+    private synchronized boolean isIdentityInList(String identity){
+        ArrayList<ServerConnection> serverConnections = chatManager.getClientConnectionList();
+        for (ServerConnection serverConnection: serverConnections){
+            if (identity.equals(serverConnection.getName())){
+                return true;
+            }
+        }
+        return false;
+    }
 
     public void handle() {
         try {
@@ -29,6 +62,7 @@ public class ChatServer {
                 Socket soc = serverSocket.accept();
                 ServerConnection serverConnection = new ServerConnection(soc, chatManager, commandFactory);
                 serverConnection.start(); // connection is thread
+                generateIdentityForNewClient(serverConnection);
                 chatManager.addClientConnection(serverConnection);
             }
         } catch (IOException e) {
