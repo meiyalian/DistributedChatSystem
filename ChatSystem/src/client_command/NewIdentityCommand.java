ef390d81fd18bcded64227585d348b93c2ab613b
Ying Mao <yingmao50@gmail.com>
2021-09-05 18:41:34 +1000

    updated command line input

--- ChatSystem/src/client_command/NewIdentityCommand.java
+++ ChatSystem/src/client_command/NewIdentityCommand.java
@@ -25,16 +25,13 @@ public class NewIdentityCommand extends ClientCommand {
         if (firstTimeConnection){
             chatClient.setIdentity(identity);
             System.out.println("Connected to localhost as " + identity);
-        }
-        else if (former.equals(identity)) {
+        } else if (former.equals(identity)) {
             System.out.println("Requested identity invalid or in use");
-        }
-        else if (former.equals(chatClient.getIdentity())) {
+        } else if (former.equals(chatClient.getIdentity())) {
             // if former is equal to chatClient identity, this means that this client has changed the identity
             chatClient.setIdentity(identity);
             System.out.println(former + " is now " + identity);
-        }
-        else {
+        } else {
             // if former is not equal to chatClient identity, this means that another client has changed the identity
             System.out.println("\n" + former + " is now " + identity);
         }
